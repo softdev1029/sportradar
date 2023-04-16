4f636fe1ade6f94bd805f98e132b29b97c1e907f
Joshua Lee <softdev1029@outlook.com>
2023-04-16 09:03:59 +0300

    added docker

---
+++ docker/config.js
@@ -0,0 +1,11 @@
+const config = {
+  mysql: {
+    host: "ingest_db",
+    port: 3306,
+    username: "root",
+    password: "password",
+    database: "sportradar",
+  },
+};
+
+export default config;
